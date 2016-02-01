01daa57ceb4737568e11a33d7a82f415bd1d474e
gaoxinge <gaoxinge@gmail.com>
2016-02-01 14:28:05 +0800

    Third Version

---
+++ Calculate/Calculate.java
@@ -0,0 +1,45 @@
+import edu.princeton.cs.algs4.*;
+
+public class Calculate
+{
+	public static String CalExpNoPareFirst(String s)
+	{
+	    int a=s.indexOf("*");
+		if(a==-1) return s;
+		int b=Math.max(s.lastIndexOf("+",a),s.lastIndexOf("-",a));
+		int c=Math.min(Math.min(s.indexOf("+",a+1)==-1?s.length():s.indexOf("+",a+1),s.indexOf("-",a+1)==-1?s.length():s.indexOf("-",a+1)),s.indexOf("*",a+1)==-1?s.length():s.indexOf("*",a+1));
+        return CalExpNoPareFirst(s.substring(0,b+1)+Integer.parseInt(s.substring(b+1,a))*Integer.parseInt(s.substring(a+1,c))+s.substring(c,s.length()));
+	}
+	
+	public static String CalExpNoPareSecond(String s)
+	{
+		int a=Math.min(s.indexOf("+")==-1?s.length():s.indexOf("+"),s.indexOf("-",1)==-1?s.length():s.indexOf("-",1));
+		if(a==s.length())	 return s;
+		int b=Math.min(s.indexOf("+",a+1)==-1?s.length():s.indexOf("+",a+1),s.indexOf("-",a+1)==-1?s.length():s.indexOf("-",a+1));		
+		if(s.charAt(a)=='+') return CalExpNoPareSecond(Integer.parseInt(s.substring(0,a))+Integer.parseInt(s.substring(a+1,b))+s.substring(b,s.length()));
+		else                 return CalExpNoPareSecond(Integer.parseInt(s.substring(0,a))-Integer.parseInt(s.substring(a+1,b))+s.substring(b,s.length()));
+	}
+	
+	public static String CalExp(String s)
+	{
+		int a=s.indexOf("(");
+		if(a==-1) return CalExpNoPareSecond(CalExpNoPareFirst(s));
+		int index=0;
+		for(int i=a+1;i<s.length();i++)
+		{
+			if(s.charAt(i)=='(') index++;
+			if(s.charAt(i)==')') index--;
+			if(index==-1)        return CalExp(s.substring(0,a)+CalExp(s.substring(a+1,i))+s.substring(i+1,s.length()));
+		}
+		throw new RuntimeException("Leftparentheses are not matched with rightparentheses in the expression to be calculated");
+	}
+	
+	public static void main(String[] args)
+	{ 
+		StdOut.println(CalExpNoPareFirst("-11+1*2*3+4+5*6"));
+		
+		StdOut.println(CalExpNoPareSecond(CalExpNoPareFirst("-11+1*2*3+4+5*6")));
+		
+		StdOut.println(CalExp("-11+1*2*3+4+5*6+7*((8+9)*10)"));
+	}
+}
