48bc0eeb4276c3a99e48e9c4f1bf4e595a595e9c
gaoxinge <gaoxx5@gmail.com>
2016-03-29 08:42:38 +0800

    codewar

--- codewar_StringSort/StringSort.java
+++ codewar_StringSort/StringSort.java
@@ -2,30 +2,28 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class StringSort
-{	
-	//there exists one-liner of changes between String and char[]
-	//application: with the help of Arrays.sort, sorting a string will be quite easy
-	//NOTE: Arrays.sort does NOT accept a String variable!
-    public static String sort(String str)
+{
+	String a;
+	String[] b;
+	
+    public static String sensitive(String str)
 	{
 		if(str==null || str.isEmpty())
 			return "Ïnvalid String";
 		else
 		{
-			//Case Sensitive
-			char[] c=str.toCharArray();
-			Arrays.sort(c);
-			return new String(c);
+			String[] s=str.split("");
+			Arrays.sort(s);
+			return String.join("",s);
 		}
 	}
 	
-	public static String insort(String str)
+	public static String insensititive(String str)
 	{
 		if(str==null || str.isEmpty())
 			return "Ïnvalid String";
 		else
 		{
-			//Case Insensitive
 			String[] s=str.split("");
 			Arrays.sort(s,String.CASE_INSENSITIVE_ORDER);
 			return String.join("",s);
@@ -35,7 +33,7 @@ public class StringSort
 	public static void main(String args[]) 
 	{    
 		String s="dacbAC";
-		System.out.println(sort(s));
-		System.out.println(insort(s));
+		System.out.println(sensitive(s));
+		System.out.println(insensititive(s));
 	}			
 }
