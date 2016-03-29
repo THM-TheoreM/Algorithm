d6db90eab897769cce4739b35e81a76a2ba9e6d2
gaoxinge <gaoxx5@gmail.com>
2016-03-29 11:44:28 +0800

    codewar

--- codewar_StringSort/StringSort.java
+++ codewar_StringSort/StringSort.java
@@ -1,10 +1,27 @@
 import java.util.Arrays;
 import java.util.Comparator;
 
+class len implements Comparator<String>
+{
+	public int compare(String v, String w)
+	{
+		if(v.length()<w.length()) return -1;
+		if(v.length()>v.length()) return 1;
+		return 0;
+	}
+}
+
+class tive implements Comparator<String>
+{
+	public int compare(String v,String w)
+	{
+		if(v.toUpperCase().compareTo(w.toUpperCase())!=0) return v.toUpperCase().compareTo(w.toUpperCase());
+		return v.compareTo(w);
+	}
+}
+
 public class StringSort
 {
-	String a;
-	String[] b;
 	
     public static String sensitive(String str)
 	{
@@ -12,9 +29,9 @@ public class StringSort
 			return "Ïnvalid String";
 		else
 		{
-			String[] s=str.split("");
+			char[] s=str.toCharArray();
 			Arrays.sort(s);
-			return String.join("",s);
+			return new String(s);
 		}
 	}
 	
@@ -29,11 +46,32 @@ public class StringSort
 			return String.join("",s);
 		}
 	}
+	
+	public static String tive(String str)
+	{
+		if(str==null || str.isEmpty())
+			return "Ïnvalid String";
+		else
+		{
+			String[] s=str.split("");
+			Arrays.sort(s,new tive());
+			return String.join("",s);
+		}
+	}
+	
+	public static void len(String[] s)
+	{Arrays.sort(s,new len());}
 
 	public static void main(String args[]) 
 	{    
-		String s="dacbAC";
-		System.out.println(sensitive(s));
-		System.out.println(insensititive(s));
+		String str="dacbAC";
+		System.out.println(sensitive(str));
+		System.out.println(insensititive(str));
+		System.out.println(tive(str));
+		
+		String[] s={"asd","asdfd","a","asdfsdfas","dasd"};
+		len(s);
+		for(String x:s)
+			System.out.print(x+" ");
 	}			
 }
